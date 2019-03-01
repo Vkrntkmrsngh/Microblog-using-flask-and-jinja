a0130ada0b5d2b9874f159439dd1f20eb1502ff7
Vikrant Singh <vkrntkmrsngh@gmail.com>
2019-03-01 20:26:52 +0530

    Login Page Added

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 
-from flask import render_template
+from flask import render_template,flash,redirect
 from app import app
+from app.forms import LoginForm
 
 @app.route("/")
 @app.route("/index")
@@ -19,3 +20,12 @@ def index():
         }
     ]
     return render_template("index.html",title='Home',user=user,posts=posts)
+
+@app.route("/login", methods=['GET','POST'])
+def login():
+    form=LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {},remember me {}'.format(form.username.data,form.rememberme.data))
+        return redirect('/index')
+        
+    return render_template("login.html",form=form,title='Sign In')
